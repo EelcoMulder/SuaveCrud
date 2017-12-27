e7b30aa102da298bb9cc1185133db8cad98818e4
Eelco Mulder <eelcomulder@hotmail.com>
2017-12-27 10:48:16 +0100

    Fix transaction error

--- SuaveCrud.Web/Database.fs
+++ SuaveCrud.Web/Database.fs
@@ -1,9 +1,10 @@
 namespace SuaveCrud.Database
 open FSharp.Data.Sql
+open FSharp.Data.Sql.Transactions
 
 module DatabaseContext =
     [<Literal>]
     let ConnectionString = @"Server=.\SQLExpress;Database=Series;Integrated Security=SSPI;"
     type SqlProvider = SqlDataProvider<Common.DatabaseProviderTypes.MSSQLSERVER, ConnectionString> 
     let getContext = 
-        SqlProvider.GetDataContext()
+        SqlProvider.GetDataContext( {Timeout = System.TimeSpan.MaxValue; IsolationLevel = IsolationLevel.DontCreateTransaction})
